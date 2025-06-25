6170a1770296d104db4b1610bbabe2e509c8fc7c
Hedi-Slm <hedi.slm42@gmail.com>
2025-06-25 16:30:54 +0200

    added name field to event model

--- app/models/event.py
+++ app/models/event.py
@@ -7,6 +7,7 @@ class Event(Base):
     __tablename__ = "events"
 
     id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)  # ADD THIS LINE
     contract_id = Column(Integer, ForeignKey("contracts.id"), nullable=False)
     client_id = Column(Integer, ForeignKey("clients.id"), nullable=False)
     support_id = Column(Integer, ForeignKey("users.id"), nullable=True)
@@ -19,4 +20,4 @@ class Event(Base):
 
     contract = relationship("Contract")
     client = relationship("Client")
-    support = relationship("User")
+    support_contact = relationship("User")
