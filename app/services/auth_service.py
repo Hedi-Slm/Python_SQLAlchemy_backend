5e11422dfa7f0967fc4e228c225d558b94c4bd10
Hedi-Slm <hedi.slm42@gmail.com>
2025-07-07 08:46:08 +0200

    updated login function with try block

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -5,9 +5,10 @@ from app.models.user import User
 
 def login_user(email: str, password: str) -> User:
     db = SessionLocal()
-    user = db.query(User).filter_by(email=email).first()
-    db.close()
-
-    if user and verify_password(password, user.password):
-        return user
-    return None
+    try:
+        user = db.query(User).filter_by(email=email).first()
+        if user and verify_password(password, user.password):
+            return user
+        return None
+    finally:
+        db.close()
